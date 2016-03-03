18ba65be9727f1922658ac6bc2c154189329a5f4
Stuart McCamley <s.mccamley@itrex.co.uk>
2016-03-03 21:46:00 +0000

    removed credentials from a file that should never have been made public, performing investigation as to how this happend, possibly due to exposed server

--- config.php
+++ config.php
@@ -2,71 +2,84 @@
 	/* Begin Bootstrap */
 	require '/home/capturetheevent/vendor/autoload.php';
 
-	if(!defined('AWS_ACCESS_KEY_ID'))
-		define('AWS_ACCESS_KEY_ID',  'AKIAJPC6SYP34RJSPMLA');
+	/**
+	* 
+	*/
+	class awsConfig 
+	{
+		
+		function __construct($config)
+		{
+	
+			if(!defined('AWS_ACCESS_KEY_ID'))
+				define('AWS_ACCESS_KEY_ID',  $config['AWS_ACCESS_KEY_ID']);
 
-	if(!defined('AWS_SECRET_ACCESS_KEY'))
-		define('AWS_SECRET_ACCESS_KEY', 'WmyxUTSrI/0raUGOuYdhQfwQPzf6FdtcxD4zyi2o');
+			if(!defined('AWS_SECRET_ACCESS_KEY'))
+				define('AWS_SECRET_ACCESS_KEY', $config['AWS_SECRET_ACCESS_KEY']);
 
+			// e.g. 'http://queue.amazonaws.com'
+			define('SQS_ENDPOINT', $config['SQS_ENDPOINT']);
 
-	define('SQS_ENDPOINT', 'http://queue.amazonaws.com');
+			/* there can be only one! */
+			// e.g. 'capturedomain'
+			define('CTE_DOMAIN', $config['CTE_DOMAIN']);
 
-	/* there can be only one! */
-	define('CTE_DOMAIN', 'capturedomain');
+			// define('IMAGE_DOMAIN','imagedomain');
+			// define('GALLERY_DOMAIN', 'gallerydomain');
+			// define('UPLOAD_DOMAIN', 'uploaddomain');
+			// define('LOG_DOMAIN', 'logdomain');
 
-	// define('IMAGE_DOMAIN','imagedomain');
-	// define('GALLERY_DOMAIN', 'gallerydomain');
-	// define('UPLOAD_DOMAIN', 'uploaddomain');
-	// define('LOG_DOMAIN', 'logdomain');
+			// e.g. 'image-processing-queue'
+			define('PROCESSING_QUEUE', $config['PROCESSING_QUEUE']);
+			// e.g. 'gallery-create-queue'
+			define('GALLERY_QUEUE', $config['GALLERY_QUEUE']);
+			// e.g. 'image-process-complete-queue'
+			define('PROCESS_COMPLETE_QUEUE', $config['PROCESS_COMPLETE_QUEUE']);
 
-	define('PROCESSING_QUEUE', 'image-processing-queue');
-	define('GALLERY_QUEUE','gallery-create-queue');
-	define('PROCESS_COMPLETE_QUEUE', 'image-process-complete-queue');
+			// log states
+			define('AWS_LOG_ACTIVE','active');
+			define('AWS_LOG_SUCCESS','success');
+			define('AWS_LOG_INFO','info');
+			define('AWS_LOG_WARNING','warning');
+			define('AWS_LOG_ERROR','danger');
 
+			'https://s3-eu-west-1.amazonaws.com/cte-lowres-unprotected'
+			define('S3_UNPROTECTED_BASE', $config['S3_UNPROTECTED_BASE']);
+			'https://s3-eu-west-1.amazonaws.com/cte-highres-protected'
+			define('S3_PROTECTED_BASE', $config['S3_PROTECTED_BASE']);
 
-	// log states
-	define('AWS_LOG_ACTIVE','active');
-	define('AWS_LOG_SUCCESS','success');
-	define('AWS_LOG_INFO','info');
-	define('AWS_LOG_WARNING','warning');
-	define('AWS_LOG_ERROR','danger');
+			use Aws\Common\Aws;
 
-	define('S3_UNPROTECTED_BASE', 'https://s3-eu-west-1.amazonaws.com/cte-lowres-unprotected');
-	define('S3_PROTECTED_BASE', 'https://s3-eu-west-1.amazonaws.com/cte-highres-protected');
+			require '/home/capturetheevent/vendor/itrex/common/config.php';
 
-	use Aws\Common\Aws;
+			require 'common/aws_plus.php';
+			require 'common/aws_simple_db.php';
+			require 'common/aws_simple_gallery_db.php';
+			require 'common/aws_simple_upload_db.php';
+			require 'common/aws_simple_log_db.php';
+			require 'common/aws_simple_image_db.php';
 
-	require '/home/capturetheevent/vendor/itrex/common/config.php';
+			// e.g. region 'eu-west-1'
+			$aws = Aws::factory(array(
+				'key'    => AWS_ACCESS_KEY_ID,
+				'secret' => AWS_SECRET_ACCESS_KEY,
+				'region' => $config['region'],
+			));
 
-	require 'common/aws_plus.php';
-	require 'common/aws_simple_db.php';
-	require 'common/aws_simple_gallery_db.php';
-	require 'common/aws_simple_upload_db.php';
-	require 'common/aws_simple_log_db.php';
-	require 'common/aws_simple_image_db.php';
+			// Get client instances from the service locator by name
+			$s3client = $aws->get('s3');
+			$ec2client = $aws->get('ec2');
+			$dbclient = $aws->get('SimpleDb');
+			$sqsclient = $aws->get('Sqs');
 
-	$aws = Aws::factory(array(
-		'key'    => AWS_ACCESS_KEY_ID,
-		'secret' => AWS_SECRET_ACCESS_KEY,
-		'region' => 'eu-west-1',
-	));
+			$aws_processor = new AWS_Plus($aws, S3_PROTECTED_BASE, S3_UNPROTECTED_BASE);
+			$aws_image_db = new AWS_Simple_Image_DB($aws) ;
+			$aws_log_db = new AWS_Simple_Log_DB($aws);
+			$aws_upload_db = new AWS_Simple_Upload_DB($aws);
+			$aws_gallery_db = new AWS_Simple_Gallery_DB($aws);
 
-	// Get client instances from the service locator by name
-	$s3client = $aws->get('s3');
-	$ec2client = $aws->get('ec2');
-	$dbclient = $aws->get('SimpleDb');
-	$sqsclient = $aws->get('Sqs');
-
-	/* push server bucket */
-	$bucket = 'cte-highres-protected';
-	$public_bucket = 'cte-lowres-unprotected';
-
-	$aws_processor = new AWS_Plus($aws, $bucket, $public_bucket);
-	$aws_image_db = new AWS_Simple_Image_DB($aws) ;
-	$aws_log_db = new AWS_Simple_Log_DB($aws);
-	$aws_upload_db = new AWS_Simple_Upload_DB($aws);
-	$aws_gallery_db = new AWS_Simple_Gallery_DB($aws);
-
-/* End Bootstrap */
+		/* End Bootstrap */
+		}
+	}
 
 ?>
